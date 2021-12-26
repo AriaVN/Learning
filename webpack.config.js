dc2aa42402ca64f9010e09e1e6cb5161ee3759e3
AriaVN <Aria.veysinejad@gmail.com>
2021-12-26 18:31:05 +0330

    Compeleted Build Process

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path')
-
+/*const { config } = require('process')*/
+const currenttask = process.env.npm_lifecycle_event
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const HTMLWebpackPlugins = require('html-webpack-plugin')
+const Fse = require('fs-extra')
 const postCSSPlugins = [
     require('postcss-import'),
     require('postcss-mixins'),
@@ -8,14 +13,41 @@ const postCSSPlugins = [
     require('autoprefixer')
 ]
 
+class RunAfterCompile{
+    apply(compiler){
+        compiler.hooks.done.tap('Copy images', function(){
+            Fse.copySync('./app/assets/images','./docs/assets/images')
+        })
+    }
+}
 
-module.exports={
+let cssConfig = {
+    test: /\.css$/i,
+    use: ['css-loader',{loader: 'postcss-loader', options: {postcssOptions:{plugins: postCSSPlugins}}}]
+}
+let pages = Fse.readdirSync('./app').filter(function(file){return file.endsWith('.html')}).map(function(page){
+    return new HTMLWebpackPlugins({
+        filename: page,
+        template: `./app/${page}`
+    })
+}) 
+let myConfig = {
     entry: './app/assets/Scripts/App.js',
-    output:{
+    plugins: pages,
+    module: {
+        rules: [
+            cssConfig
+        ]
+    }
+
+}
+if (currenttask =='dev'){
+    cssConfig.use.unshift('style-loader')
+    myConfig.output = {
         filename: 'bundled.js',
         path: path.resolve(__dirname,'app')
-    },
-    devServer:{
+    }
+    myConfig.devServer = {
         onBeforeSetupMiddleware: function(app, server){
             watchFiles:['./app/**/*.html']
         },
@@ -25,15 +57,40 @@ module.exports={
         hot: true,
         port: 4950,
         host: '0.0.0.0'
-    },/**/
-    mode: 'development',
-    //watch: true ,
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader','css-loader',{loader: 'postcss-loader', options: {postcssOptions:{plugins: postCSSPlugins}}}]
-            }
-        ]
     }
+    myConfig.mode= 'development'
 }
+if (currenttask =='build'){
+    myConfig.module.rules.push({
+        test: /\.is$/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+            options:{
+                presets:['@babel.presets-env']
+            }
+        }
+    })
+    cssConfig.use.unshift(MiniCSSExtractPlugin.loader)
+    postCSSPlugins.push(require('cssnano'))
+    myConfig.output = {
+        /*filename: 'bundled.js',*/
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname,'docs')
+    }
+    myConfig.mode = 'production',
+    myConfig.optimization = {
+        splitChunks: {
+          chunks: 'all',
+        }
+    }
+    myConfig.plugins.push(
+        new CleanWebpackPlugin(),
+        new MiniCSSExtractPlugin({filename:'styles.[chunkhash].css'}),
+        new RunAfterCompile()
+        )
+}
+
+
+module.exports = myConfig
