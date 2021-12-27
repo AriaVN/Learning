a8b4513d70f4544ff5712d8cf3af6b4b67f088d5
AriaVN <Aria.veysinejad@gmail.com>
2021-12-27 15:30:38 +0330

    edited Cloud Function

--- cloud-function/secret-area.js
+++ cloud-function/secret-area.js
@@ -1,6 +1,23 @@
 exports.handler = function(event, context, callback){
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the super secret area."
-    })
+    const secretContent = `
+    <h3>Welcome to the secret area</h3>
+    <p>Here we can tell you that the sky is dark, and 2+2=5. </p>
+    `
+    let inputBody
+
+    if (event.body){
+        inputBody=JSON.parse(event.body);
+    }else{
+        inputBody={};
+    }
+    if(inputBody.password == "strongPassword"){
+        callback(null, {
+            statusCode: 200,
+            body: "Welcome to the super secret area."
+        })
+    }else{
+        callback(null,{
+            statusCode:401
+        })
+    }
 }
